5afde2715f57dd1121224033e49d85b533be8f3a
Phquaryn <Karenedyer@gmail.com>
2015-06-21 11:39:10 -0400

    stage3 work session3

--- my_code.py
+++ my_code.py
@@ -1,7 +1,4 @@
-import  pywapi
-import string
 
-noaa_result = pywapi.get_weather_from_noaa('KALB')
 
 def generateHTML(topic,description):
     html_text_1 = '''
@@ -18,5 +15,4 @@ def generateHTML(topic,description):
     full_html_text = html_text_1 + html_text_2 + html_text_3
     return full_html_text
 
-print generateHTML("How is the weather? ", "How's the weather? ")
-print "NOAA says: It is " + string.lower(noaa_result['weather']) + " and " + noaa_result['temp_f'] + "F now in Albany, NY.\n"
+print generateHTML("How are you?","How's the weather?")
